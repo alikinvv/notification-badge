9e19a567edcf9eb4d06a27d8a71f8aea286823ba
Valery Alikin <weblcw@gmail.com>
2019-03-14 15:14:13 +0500

    Initial commit

---
+++ build/js/main.js
@@ -0,0 +1,43 @@
+$(function () {
+// ff2900
+  for (var i = 0; i < 70; i++) {
+    $('.bubbles').append('<circle r="' + getRandomInt(5, 15) + '" cy="300" fill="#ff2900" cx="' + getRandomInt(40, 110) + '"></circle>');
+  }
+
+  var html = $('.bubbles').html();
+
+  $('.bubbles').html(html);
+
+  function getRandomInt (min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+
+    $('.bubbles circle').each(function () {
+        var del = getRandomInt(0, 1000 * 3);
+        var xv = getRandomInt(-35, 35);
+        
+        anime({
+            targets: $(this)[0], 
+            r: {
+                value: 0,
+                duration: 500,
+                delay: del + 1000
+            },   
+            translateX: {
+                value: xv,
+                duration: 800,
+                delay: del,
+            },     
+            translateY: {
+                value: [0, -150],
+                duration: 2000,
+                delay: del,
+            },            
+            loop: true,
+            easing: 'linear'
+        })
+    });
+});
